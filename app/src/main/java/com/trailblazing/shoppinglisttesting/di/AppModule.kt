0e0671a61d4eaa3ddd3d665b0847177dce59f0dc
Jaineel-Mamtora <jaineelmamtora99@gmail.com>
2022-05-06 11:48:16 +0530

    initial commit

---
+++ app/src/main/java/com/trailblazing/shoppinglisttesting/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.trailblazing.shoppinglisttesting.di
+
+import android.content.Context
+import androidx.room.Room
+import com.trailblazing.shoppinglisttesting.data.local.ShoppingItemDatabase
+import com.trailblazing.shoppinglisttesting.data.remote.PixabayAPI
+import com.trailblazing.shoppinglisttesting.other.Constants.BASE_URL
+import com.trailblazing.shoppinglisttesting.other.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideShoppingDao(
+        database: ShoppingItemDatabase
+    ) = database.shoppingDao()
+
+    @Singleton
+    @Provides
+    fun providesPixabayAPI(): PixabayAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayAPI::class.java)
+    }
+}
